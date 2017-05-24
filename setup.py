82d5a6cac60cc9fcd14f4ab9f6717dddf4f02ca8
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2017-05-24 15:21:54 +0200

    Adds fix to setup.py
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import os
 
 def _post_install(libname, libpath):
     from JumpScale9 import j
-    import os
     j.tools.jsloader.copyPyLibs()
 
     # ensure plugins section in config
@@ -14,7 +13,7 @@ def _post_install(libname, libpath):
         j.application.config['plugins'] = []
 
     # add this plugin to the config
-    c = j.core.state.configGet('plugins', defval={})
+    c = j.core.state.configGet('plugins', defval=[])
     exists = any([x for x in c if x == libname])
     if not exists:
         c.append({libname: libpath})
