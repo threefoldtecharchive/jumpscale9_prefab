21f6f53e8f0e543702398257fd8ee80a107eec62
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2017-06-01 08:37:25 +0200

    Fix pregabG8OSUfs
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- JumpScale9Prefab/systemservices/PrefabG8OSUfs.py
+++ JumpScale9Prefab/systemservices/PrefabG8OSUfs.py
@@ -1,9 +1,9 @@
 from js9 import j
 
-app = j.tools.cuisine._getBaseAppClass()
+app = j.tools.prefab._getBaseAppClass()
 
 
-class CuisineG8OSUfs(app):
+class PrefabG8OSUfs(app):
     """
     """
     NAME = 'g8ufs'
@@ -12,11 +12,11 @@ class CuisineG8OSUfs(app):
         if reset is False and self.isInstalled():
             return
 
-        self.cuisine.package.mdupdate()
-        self.cuisine.package.install('build-essential')
+        self.prefab.package.mdupdate()
+        self.prefab.package.install('build-essential')
 
-        self.cuisine.development.golang.get("github.com/g8os/g8ufs")
-        self.cuisine.core.file_copy("$GOPATHDIR/bin/g8ufs", "$BASEDIR/bin/")
+        self.prefab.development.golang.get("github.com/g8os/g8ufs")
+        self.prefab.core.file_copy("$GOPATHDIR/bin/g8ufs", "$BASEDIR/bin/")
 
         if install:
             self.install(start)
