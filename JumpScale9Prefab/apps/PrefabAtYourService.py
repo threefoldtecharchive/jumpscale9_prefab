10de6fd645ebde3eec557e82c14a33a3d64ab135
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2017-06-14 14:04:08 +0200

    Change cockpit,ays prefab modules to allow configuring the portal with ays

--- JumpScale9Prefab/apps/PrefabAtYourService.py
+++ JumpScale9Prefab/apps/PrefabAtYourService.py
@@ -1,11 +1,14 @@
 from js9 import j
-from time import sleep
-
 
 base = j.tools.prefab._getBaseClass()
 
 
 class PrefabAtYourService(base):
+    def _init(self):
+        self.base_dir = j.sal.fs.joinPaths(
+            self.prefab.core.dir_paths["JSAPPSDIR"],
+            "atyourservice"
+        )
 
     def configure(self, production=False, client_secret='', client_id='', organization='', redirect_address='locahost:5000'):
         jwt_key = "-----BEGIN PUBLIC KEY-----\\nMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAES5X8XrfKdx9gYayFITc89wad4usrk0n2\\n7MjiGYvqalizeSWTHEpnd7oea9IQ8T5oJjMVH5cc0H5tFSKilFFeh//wngxIyny6\\n6+Vq5t5B0V0Ehy01+2ceEon2Y0XDkIKv\\n-----END PUBLIC KEY-----\\n"
@@ -23,33 +26,28 @@ class PrefabAtYourService(base):
         j.core.state.configSave()
 
     def install(self):
-        self.prefab.development.pip.multiInstall([
-            'sanic>=0.5.2',
-            'jsonschema'
-        ])
-
-        base_dir = j.sal.fs.joinPaths('$JSAPPSDIR', 'atyourservice')
-
-        self.prefab.core.dir_ensure(base_dir)
-
+        self.prefab.core.dir_ensure(self.base_dir)
+        server_dir = j.sal.fs.joinPaths(self.base_dir, 'JumpScale9AYS/ays/server/')
+        self.prefab.core.dir_ensure(server_dir)
+        code_dir = self.prefab.core.dir_paths["CODEDIR"]
         # link apidocs and index.html
         self.prefab.core.file_link(
-            j.sal.fs.joinPaths('$CODEDIR', 'github/jumpscale/jumpscale_core9/apps/atyourservice/apidocs'),
-            j.sal.fs.joinPaths(base_dir, 'apidocs')
+            j.sal.fs.joinPaths(code_dir, 'github/jumpscale/ays9/JumpScale9AYS/ays/server/apidocs'),
+            j.sal.fs.joinPaths(server_dir, 'apidocs')
         )
 
         self.prefab.core.file_link(
-            j.sal.fs.joinPaths('$CODEDIR', 'github/jumpscale/jumpscale_core9/apps/atyourservice/index.html'),
-            j.sal.fs.joinPaths(base_dir, 'index.html')
+            j.sal.fs.joinPaths(code_dir, 'github/jumpscale/ays9/JumpScale9AYS/ays/server/index.html'),
+            j.sal.fs.joinPaths(self.base_dir, 'JumpScale9AYS/ays/server/index.html')
         )
 
         self.prefab.core.file_link(
-            j.sal.fs.joinPaths('$CODEDIR', 'github/jumpscale/jumpscale_core9/apps/atyourservice/main.py'),
-            j.sal.fs.joinPaths(base_dir, 'main.py')
+            j.sal.fs.joinPaths(code_dir, 'github/jumpscale/ays9/main.py'),
+            j.sal.fs.joinPaths(self.base_dir, 'main.py')
         )
 
     def start(self, host='localhost', port=5000):
-        cmd = 'cd $JSAPPSDIR/atyourservice; jspython main.py -h {host} -p {port}'.format(host=host, port=port)
+        cmd = 'cd {base_dir}; python3 main.py -h {host} -p {port}'.format(base_dir=self.base_dir, host=host, port=port)
         self.prefab.processmanager.ensure(name='atyourservice', cmd=cmd)
 
     def stop(self):
--- JumpScale9Prefab/solutions/PrefabCockpit.py
+++ JumpScale9Prefab/solutions/PrefabCockpit.py
@@ -5,35 +5,23 @@ base = j.tools.prefab._getBaseClass()
 
 class PrefabCockpit(base):
 
-    def install(self, start=True, branch="8.2.0", reset=False, ip="localhost", production=False,
+    def install(self, start=True, branch="master", reset=False, ip="localhost", production=False,
                 client_id='', ays_secret='', portal_secret='', organization=''):
         """
-        This will install the all the component of the cockpit in one command.
-        (mongodb, portal, atyourservice)
-
         production: cockpit mode (atyourservice and portal in development mode if False)
-
         client_id: oauth client id
         ays_secret: client secret of ays
         portal_secret: client secret of portal
         organization: oauth organization
-
-        Make sure that you don't have uncommitted code in any code repository cause this method will discard them !!!
         """
-        self.install_deps()
-        # install mongodb, required for portal
-        self.prefab.apps.mongodb.install(start=start, reset=reset)
 
         # install portal
         self.prefab.apps.portal.install(start=False, branch=branch, reset=reset)
         # add link from portal to API
-        # 1- copy the nav to the portalbase and then edit it
-        content = self.prefab.core.file_read(
-            '$CODEDIR/github/jumpscale/jumpscale_portal8/apps/portalbase/AYS81/.space/nav.wiki')
-
-        # 2- fix the ays api endpoint.
+        content = self.prefab.core.file_read('$CODEDIR/github/jumpscale/portal9/apps/portalbase/AYS/.space/nav.wiki')
         content = content.replace('AYS API:http://localhost:5000', "AYS API:http://{ip}:5000".format(ip=ip))
-        self.prefab.core.file_write('$JSAPPSDIR/portals/main/base/AYS81/.space/nav.wiki', content=content)
+        self.prefab.core.dir_ensure('$JSAPPSDIR/portals/main/base/AYS/.space/')
+        self.prefab.core.file_write('$JSAPPSDIR/portals/main/base/AYS/.space/nav.wiki', content=content)
 
         self.prefab.apps.portal.stop()
         self.prefab.apps.portal.configure(
@@ -44,6 +32,8 @@ class PrefabCockpit(base):
             redirect_address='%s:8200' %
             ip)
 
+        # Configure AYS
+        # Install ays will just link apidocs, index.html, main.py from $CODEDIR into $JSAPPSDIR
         self.prefab.apps.atyourservice.install()
         self.prefab.apps.atyourservice.stop()
         self.prefab.apps.atyourservice.configure(
@@ -55,9 +45,10 @@ class PrefabCockpit(base):
             ip)
 
         # configure base URI for api-console
-        raml = self.prefab.core.file_read('$JSAPPSDIR/atyourservice/apidocs/api.raml')
+        raml_path = "$JSAPPSDIR/atyourservice/JumpScale9AYS/ays/server/apidocs/api.raml"
+        raml = self.prefab.core.file_read(raml_path)
         raml = raml.replace('baseUri: http://localhost:5000', "baseUri: http://{ip}:5000".format(ip=ip))
-        self.prefab.core.file_write('$JSAPPSDIR/atyourservice/apidocs/api.raml', raml)
+        self.prefab.core.file_write(raml_path, raml)
 
         if start:
             # start API and daemon
