e906b0a38c5bbc25e8cf747fb94464f439a84d9d
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-22 17:01:07 +0200

    stop setting GIGBRANCH env variable

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -5,8 +5,6 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/main
 export SSHKEYNAME=main
 
 export GIGSAFE=1
-# export GIGBRANCH=$(git symbolic-ref --short HEAD)
-export GIGBRANCH=master
 export GIGDEVELOPERBRANCH=master
 
 curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
