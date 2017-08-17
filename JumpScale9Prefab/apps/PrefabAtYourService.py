5eaf4991fcb9dcee8c1c3f90e7e30d4bdda5dd49
alichaddad <chaddada@greenitglobe.com>
2017-08-17 16:12:02 +0200

    changes for consistency

--- JumpScale9Prefab/apps/PrefabAtYourService.py
+++ JumpScale9Prefab/apps/PrefabAtYourService.py
@@ -32,9 +32,9 @@ class PrefabAtYourService(base):
         code_dir = self.prefab.core.dir_paths["CODEDIR"]
         if install_portal:
             self.prefab.apps.portal.install()
-        if j.sal.fs.exists('/opt/jumpscale9/apps/portals'):
-            self.prefab.apps.portal.addSpace('%s/github/jumpscale/ays9/apps/AYS' % code_dir)
-            self.prefab.apps.portal.addActor('%s/github/jumpscale/ays9/apps/ays_tools' % code_dir)
+        if j.sal.fs.exists('{}/portals'.format(self.prefab.core.dir_paths["JSAPPSDIR"])):
+            self.prefab.apps.portal.addSpace('{}/github/jumpscale/ays9/apps/AYS'.format(code_dir))
+            self.prefab.apps.portal.addActor('{}/github/jumpscale/ays9/apps/ays_tools'.format(code_dir))
 
     def install(self, install_portal=False):
         self.prefab.core.dir_ensure(self.base_dir)
