52aade16cf58a28cf82bb77dd2fbe97856b414c1
alichaddad <chaddada@greenitglobe.com>
2017-09-25 08:37:04 +0200

    improved module implementation

--- JumpScale9Prefab/apps/PrefabCelery.py
+++ JumpScale9Prefab/apps/PrefabCelery.py
@@ -2,19 +2,29 @@ from js9 import j
 
 base = j.tools.prefab._getBaseAppClass()
 
-class PrefabCelery(base):
 
-    def _init(self):
-        self.code_dir = '/opt/code/github/jumpscale/srobot'
+class PrefabCelery(base):
 
     def install(self):
         self.prefab.development.pip.install('celery[redis]')
         self.prefab.development.pip.install('flower')
-        j.clients.git.pullGitRepo(url='git@github.com:Jumpscale/srobot.git')
         j.clients.redis.start4core()
 
-    def start(self):
-        cmd = 'celery -A tasks worker --loglevel=info'
-        self.prefab.processmanager.ensure('celery1', cmd=cmd, path=self.code_dir)
-        cmd = 'celery -A tasks flower --address=0.0.0.0 --port=5555 --broker=redis://localhost:6379'
-        self.prefab.processmanager.ensure('celery2', cmd=cmd, path=self.code_dir)
+    def start(self, cmd, path="$JSAPPSDIR/celery/tasks.py", broker='redis://localhost:6379', appname='celery'):
+        """
+        :param cmd: contains the arg and its parameters, for example `worker --loglevel=info`
+        :param path: path of the module defining the celery app, default is $JSAPPSDIR/celery/tasks.py
+        :param broker: specify the celery broker, default redis
+        :param appname: name of celery app
+        """
+        parent = j.sal.fs.getParent(path)
+        module = j.sal.fs.getBaseName(path).split('.py')[0]
+        if parent == '$JSAPPSDIR/celery' and not self.prefab.core.exists('$JSAPPSDIR/celery/tasks.py'):
+            content = """
+from celery import Celery
+app = Celery('{name}', broker='{back}', backend='{back}')
+            """.format(back=broker, name=appname)
+            self.prefab.core.dir_ensure(parent)
+            self.prefab.core.file_write(path, content)
+        cmd = 'celery -A {module} {cmd} --broker={broker}'.format(module=module, cmd=cmd, broker=broker)
+        self.prefab.processmanager.ensure(appname, cmd=cmd, path=parent)
