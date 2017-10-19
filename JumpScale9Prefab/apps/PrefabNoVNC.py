8bfdbbfe4c1705858cb5239d63635ad20c8054ae
Sarah Boctor <boctor.sarah@gmail.com>
2017-10-19 12:14:36 +0200

    Make the branch configurable in noVNC

--- JumpScale9Prefab/apps/PrefabNoVNC.py
+++ JumpScale9Prefab/apps/PrefabNoVNC.py
@@ -7,9 +7,9 @@ app = j.tools.prefab._getBaseAppClass()
 class PrefabNoVNC(app):
     NAME = "novnc"
 
-    def install(self, install=True, start=True, reset=False):
+    def install(self, reset=False, branch="0.5.1"):
         if reset is False and self.isInstalled():
             return
 
         if self.prefab.core.isUbuntu:
-            self.prefab.development.git.pullRepo("https://github.com/gigforks/noVNC", branch="0.5.1")
+            self.prefab.development.git.pullRepo("https://github.com/gigforks/noVNC", branch=branch)
