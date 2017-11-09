087829e83cd563196bdead1310aadc7c7636f136
Abdulrahman <tareka@codescalers.com>
2017-11-09 19:35:38 +0200

    new approach using kubeadm

--- modules/virtualization/PrefabKubernetes.py
+++ modules/virtualization/PrefabKubernetes.py
@@ -47,7 +47,7 @@ class PrefabKubernetes(app):
             return
 
         # install requirement for the running kubernetes basics
-        self.prefab.system.package.install('mercurial,conntrack')
+        self.prefab.system.package.install('mercurial,conntrack,ntp')
         self.prefab.runtimes.golang.install()
         self.prefab.virtualization.docker.install(branch='1.12')
 
@@ -57,182 +57,75 @@ class PrefabKubernetes(app):
 
         self.doneSet("install_dependencies")
 
-    def install(self, reset=False):
+    def install_base(self, reset=False):
         """
         Builds the kubernetes binaries and Moves them to the appropriate location
 
         @param reset,, bool will default to false, if ture  will rebuild even if the code has been run before.
         """
-        if self.doneCheck("install", reset):
-            return
-
-        self.install_dependencies()
-        path = '%s/kubernetes' % j.dirs.TMPDIR
-
-        # get and expand tar
-        self.prefab.core.file_download(
-            'https://github.com/kubernetes/kubernetes/releases/download/v1.7.10/kubernetes.tar.gz',
-            to='%s/kubernetes.tar.gz' % j.dirs.TMPDIR)
-        self.prefab.core.file_expand(
-            '%s/kubernetes.tar.gz' % j.dirs.TMPDIR, j.dirs.TMPDIR)
-
-        # get binaries
-        self.prefab.bash.envSet('KUBERNETES_SKIP_CONFIRM', 'true')
-        self.prefab.core.run(
-            'cd %s/cluster && bash get-kube-binaries.sh' % path)
-        self.prefab.core.run(
-            'cd %s/server && tar xf kubernetes-server*' % path)
-
-        kube_binary_path = '%s/server/kubernetes/server/bin' % path
-
-        for bin_path in self.prefab.core.find(kube_binary_path, False, executable=True):
-            self.prefab.core.file_copy(bin_path, j.dirs.BINDIR)
-
-        # build  kube-apiserver, kube-controller-manager, kube-scheduler docker images
-        for image_path in self.prefab.core.find(kube_binary_path, False, pattern='*.tar'):
-            self.prefab.core.run('docker load -i %s' % image_path)
-
-
-        # build etcd image
-        self.prefab.core.run('cd %s/cluster/images/etcd && make' % path)
-        self.prefab.bash.envSet('TAG', '1.7.10')
-        self.prefab.bash.envSet(
-            'ETCD_IMAGE', 'gcr.io/google_containers/etcd-amd64')
-
-        # build
-        self.doneSet("install")
-
-
-    def configure_networking(self, reset, working_ip=None, kube_cidr='10.0.0.0/16'):
-        if self.doneCheck("configure_netwokring", reset):
+        if self.doneCheck("install_base", reset):
             return
 
         if not self.prefab.core.isUbuntu:
-            raise RuntimeError('Only ubuntu system networking is supported at the moment, manual setup will still work')
+            raise RuntimeError('Only ubuntu systems are supported at the moment.')
 
-        # clean up docker bridge
-        self.prefab.core.run('docker network rm bridge')
-        docker_deamon_config = j.data.serializer.json.dumps({'bridge': 'cbr0', 'iptables': False, 'ip-masq': False })
-        self.prefab.core.file_write('/etc/docker/daemon.json', docker_deamon_config)
-
-        # find physical interface or specified interface make a slave of the new bridge allow bridge to take aquire ip
-        # set up new bridge to route traffic through and connect to docker deamon
-        used_interface_name = None
-        for device in self.prefab.core.getNetworkInfoGenrator():
-            if working_ip:
-                if device['ip'] == working_ip:
-                    used_interface_name = device['name']
-                    break
-
-            if not getattr(self.prefab.executor, 'addr', None):
-                raise RuntimeError('No connected ip specified , please provide internet connected ip.')
-            if device['ip'] == getattr(self.prefab.executor, 'addr', ''):
-                used_interface_name = device['name']
-                break
-
-        _interfaces_script = """
-        auto lo
-        iface lo inet loopback
-
-        auto {interface}
-        #iface ens3 inet manual
-
-        auto cbr0
-        iface cbr0 inet dhcp
-        bridge_ports {interface}
-        """.format(interface=used_interface_name)
-        self.prefab.core.file_write('/etc/network/interfaces.d/kube.cfg', _interfaces_script)
-        self.prefab.core.run('service networking restart')
-        timer = time.time()+20
-        while time.time() < timer:
-            if self.prefab.executor.test():
-                break
-        else:
-            raise RuntimeError("Something went wrong in the network configuration check 'journalctl -u networking' ")
-
-        self.prefab.core.run('service stop docker')
-        self.prefab.bash.envSet('SERVICE_CLUSTER_IP_RANGE', kube_cidr)
-
-
-#     def define_configuration(self, apiversion='v1', clusters=None, contexts=None, users=None):
-#         """
-#         Creates the configuration file for kubernetes
-#         @param clusters,,list(dict) define cluster configs,
-
-#         example :
-#         [
-#             {"cluster": {
-#                         "certificate-authority": "/etc/kubernetes/ca.pem",
-#                         "server": "https://node0.c.kubestack.internal:6443"
-#                         },
-#             "name": "kubernetes"
-#             }
-#         ]
-
-#         @param contexts,,list(dict) define context configs,
-
-#         example :
-#         [
-#         { "context": { "cluster": "kubernetes", "user": "kubelet"},
-#           "name": "kubelet-to-kubernetes" }
-#         ]
-
-#         @param users,, list(dict) define  users configs,
-
-#         example :
-#         [
-#         {
-#             "name": "kubelet",
-#             "user": {
-#                 "client-certificate": "/etc/kubernetes/client.pem",
-#                 "client-key": "/etc/kubernetes/client-key.pem"
-#             }
-#         }
-#         ]
-#         TODO
-
-#         """
-# #         delete_docker_net_cmd = """
-# # iptables -t nat -F
-# # ip link set docker0 down
-# # ip link delete docker0
-# #         """
-# #         self.prefab.core.execute_bash(delete_docker_net_cmd, profile=True)
-#         if users:
-#             pass
-#         if clusters:
-#             pass
-#         if contexts:
-#             pass
-
-#         config={
-#             'apiversion': apiversion,
-#             'clusters': clusters,
-#             'users': users
-#         }
-
-    def start_node(self, reset=True, auth_path=None, labels={}):
+        self.install_dependencies()
+        script_content = """
+        apt-get update && apt-get install -y apt-transport-https
+        curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+        cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+        deb http://apt.kubernetes.io/ kubernetes-xenial main
+        EOF
         """
-        Registers the current prefab connection as a new node to the defined master
+        self.prefab.core.run(script_content)
+        self.prefab.system.package.mdupdate()
+        self.prefab.system.packages.install('kubelet,kubeadm,kubectl')
 
-        @param reset,, bool will default to false, if ture  will rebuild even if the code has been run before.
-        @param auth_path,,str  Path to credentials to authenticate itself to the apiserver.
-        @param  labels,, dict(str, str) Labels to add when registering the node in the cluster.
-        TODO
-        """
-        if self.doneCheck("install", reset):
+
+        # build
+        self.doneSet("install_base")
+
+
+    def  install_master(self, reset=False, kube_cidr='10.0.0.0/16', flannel=True):
+        if self.doneCheck("install_master", reset):
             return
 
-        pm=self.prefab.system.processmanager.get()
-        if auth_path:
-            pass
-        if labels:
-            pass
+        if flannel:
+            kube_cidr = '10.244.0.0/16'
 
-        cmd='kubelet '
-        pm.enusre(name='kubelet',)
+        self.install_base()
 
-        self.doneSet("install")
+        rc, out, err = self.prefab.core.run('kubeadm init --pod-network-cidr=%s' % kube_cidr)
+        if rc != 0:
+            raise RuntimeError(err)
+        for line in reversed(out.splitlines()):
+            if line.startswith('  kubeadm join --token'):
+                join_line = line
+                break
 
-    def start_master(self, reset, auth_path, labels={}):
-        pass
+        self.prefab.core.run(
+            'kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml')
+
+        log_message = """
+        please wait until kube-dns deplyments are deployed before joining new nodes to the cluster.
+        to check use this use 'kubectl get pods --all-namepspaces'
+        then pass the join line returned string to the install_minion
+        """
+        print(log_message)
+
+        return join_line
+
+
+        # build
+        self.doneSet("install_master")
+
+
+    def  install_minion(self, join_line, reset=False):
+        if self.doneCheck("install_minion", reset):
+            return
+
+        self.install_base()
+        self.prefab.core.run(join_line.strip())
+
+        # build
+        self.doneSet("install_minion")
