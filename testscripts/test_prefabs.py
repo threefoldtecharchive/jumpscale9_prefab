efda5f1d08a5464e65b370ecc83a1a1816147603
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-11-22 23:09:11 +0100

    fixes

--- testscripts/test_prefabs.py
+++ testscripts/test_prefabs.py
@@ -9,27 +9,34 @@ def prefab_module(module, name):
             print("\t", method, name)
             getattr(module, method)()
 
-def runInParallel(*fns):
+def runInParallel(fns):
     proc = []
     for fn in fns:
-        p = Process(target=fn)
+        p = Process(target=fn[0], args=fn[1:])
         p.start()
         proc.append(p)
     for p in proc:
         p.join()
+        
+def main():
+    to_run = list()
+    for category in [item for item in dir(j.tools.prefab.local) if not item.startswith('_') and 'core' not in item]:
+        cat = getattr(j.tools.prefab.local, category)
+        for module in [item for item in dir(cat) if not item.startswith('_')]:
+            mod = getattr(cat, module)
+            to_run.append((prefab_module, mod, module))
+        # if category.startswith('_'):
+        #     continue
+        # cat = getattr(j.tools.prefab.local, category)
+        # for module in dir(cat):
+        #     if module.startswith('_'):
+        #         continue
+        #     if 'core' in category:
+        #         continue
+        #     mod = getattr(cat, module)
+        #     to_run.append((prefab_module, mod, module))
+    return to_run
+    runInParallel(to_run)
 
 if __name__ == '__main__':
-    to_run = list()
-    for category in dir(j.tools.prefab.local):
-        if category.startswith('_'):
-            continue
-        cat = getattr(j.tools.prefab.local, category)
-        for module in dir(cat):
-            if module.startswith('_'):
-                continue
-            if 'core' in category:
-                continue
-            mod = getattr(cat, module)
-            to_run.append(prefab_module(mod, module))
-
-    runInParallel(to_run)
+    main()
