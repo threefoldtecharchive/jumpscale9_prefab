0f4aa998173137be964e2d4d2b60d72815f98388
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-11-22 17:38:53 +0100

    update run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 set -e
 set -x
-
-sudo -HE bash -c "ssh-keygen -t rsa -N \"\" -f /root/.ssh/id_rsa"
 export SSHKEYNAME=id_rsa
 
 # Start container
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/ays9nightly -i ays9"
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; container 'pip install -e /opt/code/github/jumpscale/core9'"
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; container 'pip install -e /opt/code/github/jumpscale/lib9'"
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; container 'pip install -e /opt/code/github/jumpscale/prefab9'"
-
-# Dump the environment variables as json file in a the container cfg dir
-sudo -HE bash -c "python -c 'import json, os;print(json.dumps({\"BACKEND_ENV\": dict([(key, value) for key, value in os.environ.items() if key.startswith(\"BACKEND_\")])}))' > ~/js9host/cfg/ays_testrunner.json"
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/js9_full -i js9_full"
 
 # Run tests
-sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /opt/code/github/jumpscale/prefab9; python ./testscripts/test_prefabs.py\""
+sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /opt/code/github/jumpscale/prefab9; python3 ./testscripts/test_prefabs.py\""
