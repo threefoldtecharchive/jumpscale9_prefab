65837305fb3db98646699b78d48cbfeaf1336f05
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-02-21 14:44:40 +0200

    add prefab module for 0-db

---
+++ modules/zero_os/PrefabZOS_db.py
@@ -0,0 +1,54 @@
+from js9 import j
+
+app = j.tools.prefab._getBaseAppClass()
+
+SUPPORTED_MODE = ['user', 'seq', 'direct']
+
+
+class PrefabZOS_db(app):
+    """
+    0-db key-value store
+    """
+    NAME = 'zdb'
+
+    def build(self, debug=False, start=False, install=True, reset=False):
+        if reset is False and self.isInstalled():
+            return
+
+        if self.prefab.core.isUbuntu:
+            self.prefab.system.package.mdupdate()
+            self.prefab.system.package.install('build-essential')
+
+        path = self.prefab.tools.git.pullRepo('https://github.com/zero-os/0-db')
+
+        make = "make" if debug else "make release"
+        self.prefab.core.run("cd %s && make clean && %s" % (path, make))
+
+        if install:
+            self.install(path, start)
+
+    def install(self, source=None, start=False):
+        if not source:
+            raise j.exceptions.RuntimeError("Please provide source-code path")
+
+        self.prefab.core.file_copy("%s/bin/*" % source, "$BASEDIR/bin/")
+
+        if start:
+            self.start()
+
+    def start(self, instance='main', host='localhost', port=9900, index="/tmp/zdb-index", data="/tmp/zdb-data", mode='user', verbose=True):
+        if mode not in SUPPORTED_MODE:
+            raise ValueError("mode %s is not supported" % mode)
+
+        # FIXME: add mode selection
+        pm = self.prefab.system.processmanager.get()
+        cmdline = "$BINDIR/zdb --listen %s --port %s --index %s --data %s --mode %s" % (host, port, index, data, mode)
+
+        if verbose:
+            cmdline += " -v"
+
+        pm.ensure('%s_0db' % instance, cmd=cmdline)
+
+    def stop(self, instance='main'):
+        pm = self.prefab.system.processmanager.get()
+        pm.stop('%s_0db' % instance)
