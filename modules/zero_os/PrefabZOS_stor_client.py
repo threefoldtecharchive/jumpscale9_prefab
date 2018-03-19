a8dac18a46721372c3bb6363f0ef3420709245e1
Reem Khamis <khamisr@codescalers.com>
2018-03-19 21:27:33 +0200

    Fix g8storclient builder

--- modules/zero_os/PrefabZOS_stor_client.py
+++ modules/zero_os/PrefabZOS_stor_client.py
@@ -4,5 +4,17 @@ base = j.tools.prefab._getBaseClass()
 
 
 class PrefabZOS_stor_client(base):
+    def build(self):
+        builddir = '/tmp/buildg8client'
+        self.prefab.tools.git.pullRepo('https://github.com/maxux/lib0stor', dest=builddir)
+        self.prefab.core.run('cd {}; git submodule init; git submodule update'.format(builddir))
+
+        self.prefab.system.package.ensure('build-essential libz-dev libssl-dev python3-dev libsnappy-dev')
+
+        self.prefab.lib.cmake.install()
+        self.prefab.core.run('cd {}; make -C src'.format(builddir))
+
+        self.prefab.core.run('cd {}/python/; python3 setup.py build'.format(builddir))
+
     def install(self):
-        self.prefab.runtimes.pip.install('g8storclient')
+        return self.build()
