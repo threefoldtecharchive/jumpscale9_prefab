e0ef6a14c2d77e671883836e5cc326113d9364f2
Reem Khamis <khamisr@codescalers.com>
2018-03-20 12:56:55 +0200

    Use Maxime's wheel for lib0stor

--- modules/zero_os/PrefabZOS_stor_client.py
+++ modules/zero_os/PrefabZOS_stor_client.py
@@ -4,17 +4,5 @@ base = j.tools.prefab._getBaseClass()
 
 
 class PrefabZOS_stor_client(base):
-    def build(self):
-        builddir = '/tmp/buildg8client'
-        self.prefab.tools.git.pullRepo('https://github.com/maxux/lib0stor', dest=builddir)
-        self.prefab.core.run('cd {}; git submodule init; git submodule update'.format(builddir))
-
-        self.prefab.system.package.ensure('build-essential libz-dev libssl-dev python3-dev libsnappy-dev')
-
-        self.prefab.lib.cmake.install()
-        self.prefab.core.run('cd {}; make -C src'.format(builddir))
-
-        self.prefab.core.run('cd {}/python/; python3 setup.py build'.format(builddir))
-
     def install(self):
-        return self.build()
+        return self.prefab.runtimes.pip.install('http://home.maxux.net/wheelhouse/g8storclient-1.0-cp35-cp35m-manylinux1_x86_64.whl')
