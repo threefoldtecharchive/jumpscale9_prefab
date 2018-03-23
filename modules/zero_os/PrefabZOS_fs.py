ec3dddec0c040eb2b79b972bef1b28c153dc040d
despiegk <kristof@incubaid.com>
2018-03-23 09:30:32 +0200

    new

--- modules/zero_os/PrefabZOS_fs.py
+++ modules/zero_os/PrefabZOS_fs.py
@@ -24,7 +24,7 @@ class PrefabZOS_fs(app):
 
     def install(self, start=False):
         """
-        download, install, move files to appropriate places, and create relavent configs
+        download, install, move files to appropriate places, and create relevant configs
         """
         content = """
         [[mount]]
