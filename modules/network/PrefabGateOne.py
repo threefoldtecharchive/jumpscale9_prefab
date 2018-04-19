be76f8fb6d23b1726daa2075ec9dec07311e27e6
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2018-04-19 13:31:45 +0200

    Fix gateone prefab

--- modules/network/PrefabGateOne.py
+++ modules/network/PrefabGateOne.py
@@ -1,6 +1,4 @@
 from js9 import j
-import os
-import textwrap
 
 app = j.tools.prefab._getBaseAppClass()
 
@@ -8,56 +6,53 @@ app = j.tools.prefab._getBaseAppClass()
 class PrefabGateOne(app):
     NAME = "gateone"
 
-
     def build(self, reset=False):
         """
-        Builds GateOne
-
-        @param reset boolean: forces the build operation.
+        Build Gateone
+        :param reset: reset build if already built before
+        :return:
         """
         if self.doneCheck("build", reset):
             return
 
         self.prefab.tools.git.pullRepo("https://github.com/liftoff/GateOne", branch="master")
 
-
         self.doneSet('build')
-
 
     def install(self, reset=False):
         """
         Installs gateone
 
-        @param reset boolean: forces the install operation.
+        @param reset: boolean: forces the install operation.
     
         """
         if reset is False and self.isInstalled():
             return
 
-        self.prefab.runtimes.pip.ensure()
-        self.prefab.runtimes.pip.multiInstall(['tornado', 'pillow'])
-
-        cmd = "cd /opt/code/github/liftoff/GateOne && python setup.py install && cp /usr/local/bin/gateone $BINDIR/gateone"
+        cmd = """
+cd /opt/code/github/liftoff/GateOne
+apt-get install build-essential python3-dev python3-setuptools -y
+python3 setup.py install
+cp /usr/local/bin/gateone $BINDIR/gateone
+"""
         self.prefab.core.run(cmd)
 
         self.doneSet('install')
-
 
     def start(self, name="main", address="localhost", port=10443):
 
         """
         Starts gateone.
 
-        @param name str: instance name.
-        @param address str: bind address.
-        @param port int: port number. 
+        @param name: str: instance name.
+        @param address: str: bind address.
+        @param port: int: port number.
 
         """
 
-        cmd = "gateone --address={} --port={}".format(address, port)
+        cmd = "gateone --address={} --port={} --disable_ssl".format(address, port)
         pm = self.prefab.system.processmanager.get()
         pm.ensure(name='gateone_{}'.format(name), cmd=cmd)
-
 
     def stop(self, name='main'):
         """
@@ -66,7 +61,6 @@ class PrefabGateOne(app):
         pm = self.prefab.system.processmanager.get()
         pm.stop(name='gateone_{}'.format(name))
 
-
     def restart(self, name="main"):
         """
         Restart GateOne instance by name.
@@ -74,10 +68,8 @@ class PrefabGateOne(app):
         self.stop(name)
         self.start(name)
 
-
     def reset(self):
         """
         helper method to clean what this module generates.
         """
         pass
-
