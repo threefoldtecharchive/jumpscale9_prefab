a5c14ab24015040841a0efc4966c4d0136a1f136
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2018-05-28 16:44:15 +0200

    Add option to join team for the bot

--- modules/apps/PrefabMattermostBot.py
+++ modules/apps/PrefabMattermostBot.py
@@ -9,7 +9,6 @@ class PrefabMattermostBot(app):
     def _init(self):
         self.bot_repo = "https://github.com/gigforks/mattermost_bot.git"
         self.bot_path = "{{CODEDIR}}/mattermost_bot"
-        self.url = "http://localhost:8059/api/v4"
 
     def build(self, reset=False):
         if self.doneCheck('build', reset):
@@ -19,39 +18,41 @@ class PrefabMattermostBot(app):
         self.prefab.tools.git.pullRepo(self.bot_repo, dest=self.bot_path)
         self.doneSet('build')
 
-    def install(self, bot_user, bot_email, bot_password, reset=False):
+    def install(self, bot_user, bot_email, bot_password, bot_team, port=8065, reset=False):
         """
         Build and Install mattermost bot
         :param bot_user: the name which will be used by the bot
         :param bot_email: the email which will be used for the bot user
         :param bot_password: bot user password
-        :param start: start after install
+        :param bot_team: the team which will joined by the bot user
+        :param port: the post on which mattermost server is running
         :param reset: reset building
         """
         if self.doneCheck("install", reset):
             return
         self.build(reset=reset)
-        self._configure(bot_user, bot_email, bot_password, reset=reset)
+        self._configure(bot_user, bot_email, bot_password, bot_team, port=port, reset=reset)
         cmd = "cd {bot_path} && python3 setup.py install".format(bot_path=self.bot_path)
         self.prefab.core.run(cmd)
         self.doneSet('install')
 
-    def _configure(self, bot_user, bot_email, bot_password, reset=False):
+    def _configure(self, bot_user, bot_email, bot_password, bot_team, port, reset=False):
         if self.doneCheck("configure", reset):
             return
         cmd = """
         cd /opt/mattermost/
         ./bin/platform user create --email='{bot_email}' --password='{bot_password}' --username='{bot_user}'
         ./bin/platform user verify {bot_user}
-        """.format(bot_user=bot_user, bot_password=bot_password, bot_email=bot_email)
+        ./bin/platform team add {bot_team} {bot_user}
+        """.format(bot_user=bot_user, bot_password=bot_password, bot_email=bot_email, bot_team=bot_team)
         self.prefab.core.run(cmd)
         config_file_path = "{}/local_settings.py".format(self.bot_path)
         config_data = """
-BOT_URL = {api_url}
-BOT_LOGIN = {bot_email}
-BOT_PASSWORD = {bot_password}
+BOT_URL = "http://localhost:{port}/api/v4"
+BOT_LOGIN = "{bot_email}"
+BOT_PASSWORD = "{bot_password}"
 PLUGINS = ['mattermost_bot.tftplugins']     
-""".format(api_url=self.url, bot_email=bot_email, bot_password=bot_password)
+""".format(port=port, bot_email=bot_email, bot_password=bot_password)
         self.prefab.core.file_write(config_file_path, config_data)
         self.doneSet('configure')
 
