05aa1866d892b9edadf59cd1cb878660c9c6036f
Reem Khamis <khamisr@codescalers.com>
2018-06-07 16:36:39 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ except ImportError:
 
 setup(
     name='JumpScale9Prefab',
-    version='9.4.0-rc1',
+    version='9.4.0-rc2',
     description='Automation framework for cloud workloads remote sal, sal= system abstraction layer',
     long_description=long_description,
     url='https://github.com/Jumpscaler/prefab9',
@@ -55,7 +55,7 @@ setup(
     license='Apache',
     packages=find_packages(),
     install_requires=[
-        'JumpScale9>=9.4.0-rc1',
+        'JumpScale9>=9.4.0-rc2',
         'paramiko>=1.15.3,<2.2',  # for parallel-ssh
         'asyncssh>=1.9.0',
         'pymongo>=3.4.0',
