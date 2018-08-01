dbb286923e806131eb1d5ecbe0d324d8b57e283d
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2018-08-01 15:46:26 +0200

    update travis configs

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -4,10 +4,6 @@ set -e
 sudo ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
 export SSHKEYNAME=id_rsa
 
-export ZUTILSBRANCH=${ZUTILSBRANCH:-master}
+export JUMPSCALEBRANCH=${JUMPSCALEBRANCH:-development}
 
-curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;sudo -E bash /tmp/install.sh
-
-sudo -HE bash -c "source /opt/code/github/threefoldtech/jumpscale_bash/zlibs.sh; ZKeysLoad; ZCodeGetJS"
-sudo -HE bash -c "source /opt/code/github/threefoldtech/jumpscale_bash/zlibs.sh; ZKeysLoad; ZDockerInstallLocal"
-sudo -HE bash -c "source /opt/code/github/threefoldtech/jumpscale_bash/zlibs.sh; ZKeysLoad; ZInstall_js_full"
+curl https://raw.githubusercontent.com/threefoldtech/jumpscale_core/$JUMPSCALEBRANCH/install.sh?$RANDOM > /tmp/install_jumpscale.sh;sudo -HE bash -c 'bash /tmp/install_jumpscale.sh'
